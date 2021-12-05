d354c37912e9cec025164e68af6def416d854ee9
jneirar <jorge.neira@utec.edu.pe>
2021-12-05 09:44:13 -0500

    Informe

--- backend/test.py
+++ backend/test.py
@@ -1,11 +1,13 @@
 from Comparator import Comparator
 
-comparator = Comparator(13174)
+#Total = 13174
+comparator = Comparator(12800)
+res = comparator.KNNSearchInd("steve_jobs.jpeg", 8)
 print()
-res = comparator.KNNSearchInd("steve_jobs.jpeg", 10)
+res = comparator.KNNSearch("steve_jobs.jpeg", 8)
 print()
-res = comparator.KNNSearch("steve_jobs.jpeg", 10)
+
+res = comparator.rangeSearchInd("steve_jobs.jpeg", 0.65)
 print()
-res = comparator.rangeSearchInd("steve_jobs.jpeg", 0.58)
+res = comparator.rangeSearch("steve_jobs.jpeg", 0.65)
 print()
-res = comparator.rangeSearch("steve_jobs.jpeg", 0.58)
